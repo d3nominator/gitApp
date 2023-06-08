483aee3076a922bb6b65bb7de99622f4860aa5cc
nirbhay97 <knirbhay805@gmail.com>
2023-06-08 09:23:03 +0530

    added code editor

--- public/main.js
+++ public/main.js
@@ -1,3 +1,9 @@
-function myFunction() {
-  
-}
+// pass options to ace.edit
+
+let element = document.querySelector("#editor");
+// window.alert("here is the ocde");
+
+ace.edit(element, {
+    mode: "ace/mode/javascript",
+    theme: "ace/theme/cobalt",
+})
